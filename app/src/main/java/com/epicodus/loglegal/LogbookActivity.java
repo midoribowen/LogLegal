d86171c534a418f10838ea9f213e1d8ab3962c55
Midori Bowen <midori.p.bowen@gmail.com>
2016-03-20 12:06:20 -0700

    Comment out member variables & intents.

--- app/src/main/java/com/epicodus/loglegal/LogbookActivity.java
+++ app/src/main/java/com/epicodus/loglegal/LogbookActivity.java
@@ -1,35 +1,35 @@
 package com.epicodus.loglegal;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import butterknife.Bind;
 
 public class LogbookActivity extends AppCompatActivity {
-    public static final String TAG = LogbookActivity.class.getSimpleName();
-    @Bind(R.id.date) TextView mDate;
-    @Bind(R.id.time) TextView mTime;
-    @Bind(R.id.witnesses) TextView mWitnesses;
-    @Bind(R.id.description) TextView mDescription;
-    @Bind(R.id.policeBadge) TextView mPoliceBadge;
+//    public static final String TAG = LogbookActivity.class.getSimpleName();
+//    @Bind(R.id.date) TextView mDate;
+//    @Bind(R.id.time) TextView mTime;
+//    @Bind(R.id.witnesses) TextView mWitnesses;
+//    @Bind(R.id.description) TextView mDescription;
+//    @Bind(R.id.policeBadge) TextView mPoliceBadge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logbook);
 
-        Intent intent = getIntent();
-        String date = intent.getStringExtra("date");
-        String time = intent.getStringExtra("time");
-        String witnesses = intent.getStringExtra("witnesses");
-        String description = intent.getStringExtra("description");
-        String policeBadge = intent.getStringExtra("policeBadge");
-        mDate.setText("Date: " + date);
-        mTime.setText("Time: " + time);
-        mWitnesses.setText("Witnesses: " + witnesses);
-        mDescription.setText(description);
-        mPoliceBadge.setText("Police Badge #: " + policeBadge);
+//        Intent intent = getIntent();
+//        String date = intent.getStringExtra("date");
+//        String time = intent.getStringExtra("time");
+//        String witnesses = intent.getStringExtra("witnesses");
+//        String description = intent.getStringExtra("description");
+//        String policeBadge = intent.getStringExtra("policeBadge");
+//        mDate.setText("Date: " + date);
+//        mTime.setText("Time: " + time);
+//        mWitnesses.setText("Witnesses: " + witnesses);
+//        mDescription.setText(description);
+//        mPoliceBadge.setText("Police Badge #: " + policeBadge);
     }
 }
