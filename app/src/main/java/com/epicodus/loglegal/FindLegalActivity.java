86e7627f662fdda69b17034c2b190c9343cef6ac
Midori Bowen <midori.p.bowen@gmail.com>
2016-03-20 18:08:35 -0700

    Add placeholder data to FindLegalActivity.

--- app/src/main/java/com/epicodus/loglegal/FindLegalActivity.java
+++ app/src/main/java/com/epicodus/loglegal/FindLegalActivity.java
@@ -2,12 +2,26 @@ package com.epicodus.loglegal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.CharacterPickerDialog;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class FindLegalActivity extends AppCompatActivity {
+    @Bind(R.id.lawOfficesList) ListView mLawOfficesList;
+    String[] lawOffices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_legal);
+        ButterKnife.bind(this);
+
+        lawOffices = getResources().getStringArray(R.array.law_offices);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lawOffices);
+        mLawOfficesList.setAdapter(adapter);
     }
 }
